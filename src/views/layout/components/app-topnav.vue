c6b92f98092876073fac44f7ba531ec0a83af9ba
chn001118 <chn001118@gmail.com>
2023-04-17 17:19:59 +0800

    初始化

---
+++ src/views/layout/components/app-topnav.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+<script lang="ts">
+export default { name: "AppTopnav" }
+</script>
+<template>
+    <nav class="app-topnav">
+        顶部通栏
+    </nav>
+</template>
+
+<style scoped lang="less">
+.app-topnav {}
+</style>
